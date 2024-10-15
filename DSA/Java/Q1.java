3bc7ebf560f48b7b10a0bc64d83e8c1441cb878e
SAAD-Ai-enthusiast <saadkhan10412@gmail.com>
2024-10-16 02:16:07 +0530

    Update Q1.java

--- DSA/Java/Q1.java
+++ DSA/Java/Q1.java
@@ -1,6 +1,20 @@
 public class Q1 {
     public static void generatePascalTriangle(int n) {
-        //write your code here
+        int[][] triangle = new int[n][n];
+        triangle[0][0] = 1;
+        for (int i = 1; i < n; i++) {
+            triangle[i][0] = 1; 
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+            triangle[i][i] = 1; 
+        }        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(triangle[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
