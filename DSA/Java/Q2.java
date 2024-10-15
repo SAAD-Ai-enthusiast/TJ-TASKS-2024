2fa5a87530c4bf6f486851ed86a51d217be21e53
SAAD-Ai-enthusiast <saadkhan10412@gmail.com>
2024-10-16 02:28:21 +0530

    Update Q2.java

--- DSA/Java/Q2.java
+++ DSA/Java/Q2.java
@@ -1,6 +1,14 @@
 public class Q2 {
     public static int climbStairs(int n) {
        //write your code here
+        if (n == 1) {
+            return 1;
+        }if (n == 2) {
+            return 2;
+        } else {
+            return climbStairs(n-1) + climbStairs(n-2);
+        }
+
     }
 
     public static void main(String[] args) {
